2a3de6e116df36451ead50dcf1a6f9844a24bcd0
Leonardo Betancourt <leobeta@gmail.com>
2017-02-10 11:53:19 -0500

    Agregar @JsonIgnore a la clase de pruebas

--- src/main/java/co/com/uni5d/domain/Prueba.java
+++ src/main/java/co/com/uni5d/domain/Prueba.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
@@ -14,6 +15,7 @@ import lombok.Data;
 public class Prueba {
 	
 	@Id
+	@JsonIgnore
 	private ObjectId id;
 	private int numero;
 	private String  caracter;
